0ed890a0c36e448cff19127eacfa25199254b462
Nikhitha1711 <nikhithasajjala738@gmail.com>
2025-06-21 06:16:28 +0000

    calucaltorimpl

---
+++ Day4/calculatorimpl.java
@@ -0,0 +1,15 @@
+package Training;
+
+public class CalculatorImpl implements Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Calculator.log("Hello");
+			System.out.println(Calculator.createEmail("Swetha", "Sri"));
+		}
+		@Override
+		public double add() {
+			return 0;
+		}
+
+}
